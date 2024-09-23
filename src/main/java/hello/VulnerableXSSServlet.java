ec331675ab95ad0f0120052eb740161aa2363582
sany cyrus <sanycyrus@gmail.com>
2024-09-23 14:27:58 +0530

    Update VulnerableXSSServlet.java

--- src/main/java/hello/VulnerableXSSServlet.java
+++ src/main/java/hello/VulnerableXSSServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+// trigger for comment default
 public class VulnerableXSSServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
